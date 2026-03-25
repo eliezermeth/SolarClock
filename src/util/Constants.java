9873d6c976a464fd3bcd20feb0861e1b50f17350
Eliezer Meth <63016889+eliezermeth@users.noreply.github.com>
2026-03-25 13:36:52 -0400

    Began Constants class.

---
+++ src/util/Constants.java
@@ -0,0 +1,17 @@
+package util;
+
+/**
+ * Constants that may be used by the rest of the program.
+ */
+public class Constants
+{
+    // Halachic
+    public static final int SHAOS_PER_TEKUFAH = 12;
+    public static final int CHALAKIM_PER_SHAAH = 1080;
+
+    // Millisecond timings
+    public static final long MILLIS_PER_SECOND = 1000;
+    public static final long MILLIS_PER_DAY = 86400000L;
+
+    public static final long HOURS_PER_TERMINATOR = 12;
+}
