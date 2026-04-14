8ae34045351ea59aa2b7a0d9d671357ff7f1e424
Eliezer Meth <63016889+eliezermeth@users.noreply.github.com>
2026-04-13 21:49:28 -0400

    Modified to allow lines to be defined (and set) as dotted lines.

--- src/gui/StaticLine.java
+++ src/gui/StaticLine.java
@@ -13,9 +13,14 @@ public class StaticLine
     protected int thickness;
     /** Color of the line. */
     protected Color color;
+    /** If line should be a dotted line. */
+    protected boolean dotted = false;
+    /** The <code>BasicStroke</code> style to be used by the dotted line. */
+    protected BasicStroke style;
 
     /**
-     * Information for a static line to be displayed on the analog clock.
+     * Information for a static line to be displayed on the analog clock.  Defaults to solid line, but can be changed
+     * to be <code>dotted</code>.
      * @param time <code>LocalTime</code> for when information should be calculated.
      * @param label Text for label; if text is blank, the text portion should not be displayed.
      * @param thickness Thickness of the line; if width is <code>0</code>, the line portion should not be displayed.
@@ -27,5 +32,24 @@ public class StaticLine
         this.label = label;
         this.thickness = thickness;
         this.color = color;
+    }
+
+    /**
+     * Sets line to be a dotted line with the passed parameters.
+     * @param dotLength The length of the solid sections of the line.
+     * @param spaceLength The length of the empty sections of the line.
+     */
+    public void setDotted(float dotLength, float spaceLength)
+    {
+        dotted = true;
+
+        style = new BasicStroke(
+                thickness,                      // line width
+                BasicStroke.CAP_BUTT,
+                BasicStroke.JOIN_MITER,
+                0f,
+                new float[] {dotLength, spaceLength},
+                0f
+        );
     }
 }
