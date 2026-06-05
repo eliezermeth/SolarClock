a0ca779e867e1c86a6e094738b5f9ebb7e6f307c
Eliezer Meth <63016889+eliezermeth@users.noreply.github.com>
2026-06-05 17:45:13 -0400

    Changed constants to static numbers rather than requiring Duration conversion.

--- src/util/Constants.java
+++ src/util/Constants.java
@@ -1,7 +1,5 @@
 package util;
 
-import java.time.Duration;
-
 /**
  * Constants that may be used by the rest of the program.
  */
@@ -13,15 +11,15 @@ public class Constants
 
     // Millisecond timings
     public static final long MILLIS_PER_SECOND = 1000L;
-    public static final long MILLIS_PER_DAY = 86400000L;
+    public static final long MILLIS_PER_DAY = 86_400_000L;
 
     // Nanosecond timings
     /** The number of nanoseconds in a standard hour; {@code 3,600,000,000,000}. */
-    public static final long HOUR_NANOS = Duration.ofHours(1).toNanos();
+    public static final long HOUR_NANOS = 3_600_000_000_000L;
     /** The number of nanoseconds in a standard minute; {@code 60,000,000,000}. */
-    public static final long MINUTE_NANOS = Duration.ofMinutes(1).toNanos();
+    public static final long MINUTE_NANOS = 60_000_000_000L;
     /** The number of nanoseconds in a standard second; {@code 1,000,000,000}. */
-    public static final long SECOND_NANOS = Duration.ofSeconds(1).toNanos();
+    public static final long SECOND_NANOS = 1_000_000_000L;
 
     public static final long HOURS_PER_TERMINATOR = 12;
 }
