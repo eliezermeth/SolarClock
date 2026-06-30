7946bfab4233ade83dff90f585e555ebfab3b50c
Eliezer Meth <63016889+eliezermeth@users.noreply.github.com>
2026-06-29 21:58:53 -0400

    Progress in verification; breaking certain elements into separate methods.

--- src/util/DurationTimeFormatter.java
+++ src/util/DurationTimeFormatter.java
@@ -1,10 +1,6 @@
 package util;
 
 import java.time.Duration;
-import java.time.format.DateTimeFormatter;
-import java.util.ArrayList;
-import java.util.Deque;
-import java.util.List;
 import java.util.concurrent.TimeUnit;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
@@ -32,11 +28,11 @@ public class DurationTimeFormatter
     private static final TimeUnit[] UNITS = {TimeUnit.DAYS, TimeUnit.HOURS, TimeUnit.MINUTES, TimeUnit.SECONDS};
 
     private static final Pattern CHARS =
-            Pattern.compile("^[dHms:.+0-9]+$"); // ensure no unexpected characters
+            Pattern.compile("^[dHms:.+\\-0-9]+$"); // ensure no unexpected characters
     private static final Pattern SPLIT =
             Pattern.compile("^([^.]*)(?:\\.(.*))?$"); // split time portion from (optional) fractional portion
     private static final Pattern FRACTION =
-            Pattern.compile("^\\d?\\+?$"); // if decimal point exists, validate what follows it
+            Pattern.compile("^\\d?\\+?-?$"); // if decimal point exists, validate what follows it
     private static final Pattern TOKEN =
             Pattern.compile("(d+|H+|m+|s+|:)"); // extract labeled fields
     private static final Pattern ORDER =
@@ -143,10 +139,85 @@ public class DurationTimeFormatter
         return true;
     }
 
+    private static boolean findStartingUnit(String[] parts, boolean fractionalExists)
+    {
+        char lowestUnit = findLowestUnit(parts, fractionalExists);
+
+        // ensure proper incremental order of time segments
+        TimeUnit starting = null;
+
+        // work backward on the time array
+        for (int i = 1; i < parts.length + 1; i++)
+        {
+            String part = parts[parts.length - i];
+            char expectedChar = SECTIONS[SECTIONS.length - i]; // TODO wrong
+
+            if (!part.isEmpty()) // text exists in segment
+            {
+                char first = part.charAt(0);
+                // must match expected character
+                if (first != expectedChar)
+                    throw new IllegalArgumentException("Time segment " + part + " is incorrect.");
+                // must all be the same character
+                for (int j = 1; j < part.length(); j++)
+                    if (part.charAt(j) != first)
+                        throw new IllegalArgumentException("Time segments may only be composed of a single type of unit");
+            }
+
+            starting = UNITS[UNITS.length - i];
+        }
+    }
+
+    /**
+     * Find the lowest unit in the time unit section, be it days, hours, minutes, or seconds.  Does not complete
+     * correctly if the parts section is formatted incorrectly.
+     * @param parts
+     * @param fractionalExists
+     * @return
+     */
+    private static char findLowestUnit(String[] parts, boolean fractionalExists)
+    {
+        if (fractionalExists)
+        {
+            return SECTIONS[SECTIONS.length - 1]; // 's'
+        }
+
+        // else; fractional does not exist
+        for (int i = 1; i < parts.length + 1; i++) // traverse backward
+        {
+            String part = parts[parts.length - i];
+
+            if (!part.isEmpty()) // section is not empty
+            {
+                char firstChar = part.charAt(0);
+
+                int sectionsIndex = -1; // index in SECTIONS with corresponding letter
+                // find its position in SECTIONS by traversing forward
+                for (int j = 0; j < SECTIONS.length && sectionsIndex == -1; j++)
+                    if (firstChar == SECTIONS[j])
+                        sectionsIndex = j;
+
+                // determine how far to go back in units to the beginning of parts
+                // sectionsIndex + i - 1 to get lowest unit; if beyond index; fail
+                int potentialIndex = sectionsIndex + i - 1; // subtract 1 from i to avoid counting itself
+                if (potentialIndex >= SECTIONS.length)
+                    throw new IllegalArgumentException("Time section " + part + " is in an invalid location.");
+
+                return SECTIONS[potentialIndex]; // lowest unit
+            }
+        }
+
+        // no populated section found
+        if (parts.length == SECTIONS.length)
+            return SECTIONS[SECTIONS.length - 1];
+        else // less than max extent
+            throw new IllegalArgumentException("Not enough context in time section.");
+    }
+
     public static void main(String[] args)
     {
         String[] tests = new String[] {"HH:mm:ss.1", "m:ss", "::s", ":::", "mmmm:.", ":H::ss.3+",
-                "test", "HHHH", "sss.3", ".11", "H:dd", "H:s", "::::"};
+                "test", "HHHH", "sss.3", ".11", "H:dd", "H:s", "::::", "mm:ss.1-", ":ss.1+-"};
         // last gives wrong error message (gives invalid time order), but acceptable
         Duration d = Duration.ofHours(3);
 
@@ -218,6 +289,33 @@ public class DurationTimeFormatter
                 }
                 System.out.println("\tStarting unit: " + starting);
             }
+            else // no fractional portion; don't know what the lowest is yet
+            {
+                int passed;
+                char found;
+                boolean noCharFound = true;
+
+                // traverse backward (so that can be integrated with with-fractional code
+                for (int i = 1; i < parts.length + 1 && noCharFound; i++)
+                {
+                    if (!parts[parts.length - i].isEmpty()) // section is not empty
+                    {
+                        // pull out first character as proper unit
+                        int validCharIndex = -1;
+                        char pos = parts[parts.length - i].charAt(0);
+                        for (int j = 0; j < SECTIONS.length; j++)
+                            if (pos == SECTIONS[j])
+                            {
+                                validCharIndex = j;
+                                break;
+                            }
+                    }
+                }
+            }
+            // ensure 3+ characters can only be in first slot
+            for (int i = 1; i < parts.length; i++)
+                if (parts[i].length() > 2)
+                    System.out.println("\tCannot have more than two characters in slot " + parts[i]);
 
             // validate fractional characters
             if (gFraction != null)
