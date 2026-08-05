9c92f9ae2bf00ae8bc45c9250f5e4ee640474aa5
Eliezer Meth <63016889+eliezermeth@users.noreply.github.com>
2026-08-05 19:02:19 -0400

    Added secular date to StandardClockPanel.

--- src/gui/StandardClockPanel.java
+++ src/gui/StandardClockPanel.java
@@ -7,6 +7,7 @@ import javax.swing.*;
 import java.awt.*;
 import java.time.LocalTime;
 import java.time.ZonedDateTime;
+import java.time.format.DateTimeFormatter;
 
 public class StandardClockPanel implements TimeObserver
 {
@@ -15,6 +16,12 @@ public class StandardClockPanel implements TimeObserver
 
     // Moving parts
     private final JLabel[] components = new JLabel[3]; // HH MM SS
+    private final JLabel dayDate = new JLabel();
+
+    private DateTimeFormatter formatter = DateTimeFormatter.ofPattern("EEE, MMM d");
+    // EEE = abbreviated day; EEEE = full day of week
+    // MMM = abbreviated month; MMMM = full month
+    // d = day of month (no leading zero); dd = day of month (2 digits)
 
     public StandardClockPanel(JPanel child)
     {
@@ -61,23 +68,30 @@ public class StandardClockPanel implements TimeObserver
         gbc.fill = GridBagConstraints.NONE;
         gbc.anchor = GridBagConstraints.CENTER;
         panel.add(clockPanel, gbc);
+
+        // add day / date
+        gbc.gridx = 0; gbc.gridy = 2;
+        dayDate.setText("dayOfWeek, Month dayOfMonth");
+        panel.add(dayDate, gbc);
     }
 
     public void updateStandardClock()
     {
-        updateStandardClock(clock.getCurrentTime());
+        updateStandardClock(clock.getCurrentDateTime());
     }
 
-    public void updateStandardClock(LocalTime time)
+    public void updateStandardClock(ZonedDateTime time)
     {
         components[0].setText(String.format("%02d", time.getHour()));
         components[1].setText(String.format("%02d", time.getMinute()));
         components[2].setText(String.format("%02d", time.getSecond()));
+
+        dayDate.setText(time.format(formatter)); // TODO make more efficient; don't need to refresh each time
     }
 
     @Override
     public void updateTime(ZonedDateTime time)
     {
-        updateStandardClock(time.toLocalTime());
+        updateStandardClock(time);
     }
 }
